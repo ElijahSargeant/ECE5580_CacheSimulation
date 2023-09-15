6b7c43c25f480950e2aeed6efae9c4ccdedae133
Elijah <esargeant001@gmail.com>
2023-09-15 07:13:23 -0400

    Trace Files

--- main.c
+++ main.c
@@ -1,26 +1,39 @@
 #include <stdio.h>
 
+/******* CLI INPUTS *******
+ * 
+ * 
+ * 
+*/
+
 
 int main (int argc, char *argv[])
 {
     int counter;
 
-    unsigned int address, numEntries, i;
+    long fileSize;
+
+    unsigned int address, configuredAccesses, i;
     char traceFileName[32], operation;
     FILE* fin;
     
     fin = fopen(traceFileName, "rb");
     
-    if(fin == 0) { printf(“Unable to open trace file\n”); exit(0); }
+    if(fin == 0) { printf("Unable to open trace file\n"); exit(0); }
 
-    for(i=0; i<numEntries; i++)
-    /* numEntries is the number of accesses to simulate */
+
+    /* configuredAccesses is the number of accesses to simulate */
+    for(i=0; i<configuredAccesses; i++)
     {
-        fin.read((char*)&address, 4);
-        fin.read((char*)&operation, 1);
+        //address is 4 bytes (int)
+        fread(&address, 1, 4, fin);
+        
+        //operation is 1 byte (char)
+        fread(&operation, 1, 1, fin);
         /* the variable ‘address’ now contains the address of the data being
         accessed, while the variable ‘operation’ contains the values ‘r’ for
         a read or ‘w’ for a write. */
+        
     }
   
     return 0;
